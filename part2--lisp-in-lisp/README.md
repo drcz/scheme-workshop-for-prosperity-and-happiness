78421536188655053f96df07276e4c6116690ac3
derczyk <drcz@fnord>
2016-06-16 00:37:53 +0200

    after the workshop

---
+++ part2--lisp-in-lisp/README.md
@@ -0,0 +1,15 @@
+# second workshop for prosperity and happiness -- a lisp in lisp
+
+Thank you very much for participating! Here's the code from the workshop + a little more extended language (let, cond, list -- check in the definitions).
+
+[hola v1](dynamic-lisp.scm) -- our first interpreter, with dynamic scoping,
+
+[hola v2](static-lisp.scm) -- the improved one, with static (lexical) scoping.
+
+[a bonus](Alicante-Lexical-Lisp-interpreter.scm) -- the extended version (including destructuring lambdas, let and cond), plus a few examples for it:
+
+* [vectors, matrices](little-linear-algebra.all) -- definitions of a few non-trivial procedures;
+
+* [something dark](darkness.all) -- the simplified Y combinator for function of 1 and 2 arguments;  cat darkness.all | guile Alicante-Lexical-Lisp-interpreter.scm
+
+[js eval](js-eval-kinda.js) -- an attempt at rewriting ["dynamic"] Eval in js (seems to work).
---
+++ part2--lisp-in-lisp/static-lisp.scm
@@ -0,0 +1,77 @@
+(use-modules (srfi srfi-1) (ice-9 match) (ice-9 pretty-print))
+
+(define (error msg) (pretty-print msg) #f)
+
+(define (lookup key env)
+  (let ([val (assoc key env)])
+    (if val
+	(cdr val)
+	(error `(unbound symbol ,key)))))
+
+(define (update key val env) `([,key . ,val] . ,(alist-delete key env)))
+
+(define (bool->T/nil b) (if b 'T '()))
+
+(define (primop? sym)
+  (member sym '(car cdr cons + - * / = < atom? num? print input)))
+
+(define (Eval expr env topenv)
+  (let E ([expr expr])
+    (match expr
+      ['T 'T]
+      [() '()]
+      [(? number? n) n]      
+      [('&proc args body closure) expr]
+      [(? primop? p) p]
+      [(? symbol? s) (lookup s (append env topenv))]
+      [('quote e) e]
+      [('if premise conclusion alternative)
+       (if (eq? 'T (E premise)) (E conclusion) (E alternative))]
+      [('^ args body) `(&proc ,args ,body ,env)]
+      [(rator . rand)
+       (match (map E expr)
+	 [('+ e1 e2) (+ e1 e2)]
+	 [('* e1 e2) (* e1 e2)]
+	 [('- e1 e2) (- e1 e2)]
+	 [('/ e1 e2) (* 1.0 (/ e1 e2))]
+	 [('car e) (car e)]
+	 [('cdr e) (cdr e)]
+	 [('cons e1 e2) (cons e1 e2)]
+	 [('= e1 e2) (bool->T/nil (equal? e1 e2))]
+	 [('num? e) (bool->T/nil (number? e))]
+	 [('atom? e) (bool->T/nil (not (pair? e)))]
+	 [('print e) (begin (pretty-print e) e)]
+	 [('input) (begin (write '>) (read))]
+	 [(('&proc args body closure) . vals)
+	  (Eval body (append (map cons args vals) closure) topenv)])])))
+
+
+(define (repl output topenv)
+  (begin
+    (pretty-print output)
+    (write 'REPL>)
+    (match (read)
+      [('quit) (begin (pretty-print 'Hasta-pronto!) (quit))]
+      [('def name expr)
+       (let ([val (Eval expr '() topenv)])
+	 (if val
+	     (repl `(new binding for ,name)
+		   (update name val topenv))
+	     (repl 'sorry! topenv)))]
+      [expr
+       (let ([val (Eval expr '() topenv)])
+	 (if val
+	     (repl val topenv)
+	     (repl 'sorry! topenv)))])))
+
+(repl 'READY. '())
+
+      
+
+
+ 
+
+
+
+
+
